f7bc60bd886089aeb6a548aece92c37cd3dc15d9
karthikkumarasamy <karthik.kumarasamy.5@gmail.com>
2020-05-26 20:44:12 +0530

    Subtracting and Dividing

--- calc.py
+++ calc.py
@@ -1,24 +1,11 @@
 def add(x,y)
-<<<<<<< HEAD
     pass
 
 def sub(x,y)
-    pass
-
+    return x-y
+    
 def mul(x,y)
-    pass
+    return x*y
 
 def div(x,y)
-    pass
-=======
-  pass
- 
-def sub(x,y)
-  pass
-  
-def mul(x,y)
-  pass
-  
-def div(x,y)
-  pass
->>>>>>> fb62fa73ed5cda771aa88bbc7bc744d5366fa28b
+    return x/y
